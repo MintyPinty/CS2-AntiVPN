4f03845f9f6a46fb667109e8247b37e8e91bf093
mintyx <113274478+MintyPinty@users.noreply.github.com>
2024-09-11 08:55:33 +1000

    Update CS2_AntiVPN_Config.cs

--- CS2_AntiVPN_Config.cs
+++ CS2_AntiVPN_Config.cs
@@ -11,4 +11,5 @@ public class CS2_AntiVPN_Config : BasePluginConfig
     [JsonPropertyName("Kick_Delay")] public int KickDelay { get; set; } = 5;
     [JsonPropertyName("Kick_Message")] public string Kick_Message { get; set; } = "{Default}[{Red}AntiVPN{Default}] You have been blocked from the server for using a VPN.";
     [JsonPropertyName("Kick_Message_Delay")] public int Kick_Message_Delay { get; set; } = 1;
+    [JsonPropertyName("Webhook_Url")] public string WebhookUrl { get; set; } = "";
 }

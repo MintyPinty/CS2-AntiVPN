cb5cc8f484b57f2b9c6b86e20511c075be56e2db
mintyx <113274478+MintyPinty@users.noreply.github.com>
2024-09-11 07:59:36 +1000

    Create CS2_AntiVPN_Config.cs

---
+++ CS2_AntiVPN_Config.cs
@@ -0,0 +1,14 @@
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using System.Text.Json.Serialization;
+
+namespace CS2_AntiVPN;
+
+[MinimumApiVersion(228)]
+
+public class CS2_AntiVPN_Config : BasePluginConfig
+{
+    [JsonPropertyName("Kick_Delay")] public int KickDelay { get; set; } = 5;
+    [JsonPropertyName("Kick_Message")] public string Kick_Message { get; set; } = "{Default}[{Red}AntiVPN{Default}] You have been blocked from the server for using a VPN.";
+    [JsonPropertyName("Kick_Message_Delay")] public int Kick_Message_Delay { get; set; } = 1;
+}
